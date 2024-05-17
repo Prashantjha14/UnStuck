92c436b54508ea3b6fc38fc92b876807c62de2b2
Prashantjha14 <129178920+Prashantjha14@users.noreply.github.com>
2024-05-17 14:10:50 +0530

    fix build errors

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -9,7 +9,6 @@ import {
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
-import path from "path";
 import Question from "@/database/question.model";
 
 export async function getUserById(params: GetUserByIdParams) {
@@ -72,9 +71,9 @@ export async function deleteUser(params: DeleteUserParams) {
     // Delete user from database and questions, answers, comments, etc.
 
     // get user question ids
-    const userQuestionIds = await Question.find({
-      author: user._id,
-    }).distinct("_id");
+    // const userQuestionIds = await Question.find({
+    //   author: user._id,
+    // }).distinct("_id");
 
     await Question.deleteMany({ author: user._id });
 
